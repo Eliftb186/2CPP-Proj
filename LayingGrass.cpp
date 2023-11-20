53a73ece82932dcf2bf94e3f98bb789aed99d7fa
Le_Vrai_WIWI <superwoup@gmail.com>
2023-11-20 15:40:57 +0100

    ttiles

--- LayingGrass.cpp
+++ LayingGrass.cpp
@@ -39,7 +39,7 @@ class Tuile {
   // fonction qui retourne une tuile
   void flipTuile() { reverse(tuile.begin(), tuile.end()); }
 
-  // fonction qui tourne une tuile de 90°
+  // fonction qui tourne la tuile
   void rotateTuile() {
     // Transposer la matrice
     for (size_t i = 0; i < tuile.size(); ++i) {
@@ -103,8 +103,11 @@ class Board {
   }
 
   // liste des carrés de couleurs
-  // ⬛️ ⬜️ 🟩 🟦 🟨 🟧 🟥 🟫 🟪 | 🔳
+  // ⬛️ ⬜️ 🟩 🟦 🟨 🟧 🟥 🟫 🟪 | 🔳 | ❌
 
+  /**
+   * @brief print the board
+   */
   void printBord() {
     cout << "   ";
     for (int col = 0; col < cases[0].size(); col++) {
@@ -162,8 +165,100 @@ class Board {
   }
 };
 
-class LayingGrass {};
+/**
+ * @brief this class is used to manage the tiles
+ *
+ * @param id the id of the tile
+ * @param tile the tile matrix
+ * @param playerName the name of the player who place the tile
+ *
+ * @return vector<Tiles> the list of tiles
+ */
+class LayingGrass {
+ public:
+  struct Tiles {
+    int id;
+    vector<vector<int>> tile;
+    string playerName;
+  };
 
+  /**
+   * @brief Create a Tile List object
+   *
+   * @return vector<Tiles>
+   */
+  vector<Tiles> createTileList() {
+    vector<Tiles> tileList;
+    srand(time(0));
+
+    for (int tot = 0; tot < 96; tot++) {
+      int size = rand() % 6 + 2;
+      vector<vector<int>> tileMatrix(size, vector<int>(size, 0));
+
+      // Point de départ
+      int startX = rand() % size;
+      int startY = rand() % size;
+
+      // Nombre de déplacements aléatoires
+      int numMoves = rand() % (2 * size) + (2 * size);
+
+      // Déplacements possibles : haut, bas, droite, gauche
+      int directions[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+      int currentX = startX;
+      int currentY = startY;
+
+      for (int move = 0; move < numMoves; move++) {
+        // Choisir une direction aléatoire
+        int randomDirection = rand() % 4;
+        int newX = currentX + directions[randomDirection][0];
+        int newY = currentY + directions[randomDirection][1];
+
+        // Vérifier si la nouvelle position est valide
+        if (newX >= 0 && newX < size && newY >= 0 && newY < size) {
+          tileMatrix[newX][newY] = 1;
+          currentX = newX;
+          currentY = newY;
+        }
+      }
+
+      Tiles tile;
+      tile.id = tot;
+      tile.tile = tileMatrix;
+      tileList.push_back(tile);
+    }
+
+    return tileList;
+  }
+
+  /**
+   * @brief print the tile list
+   *
+   * @param tileList the list of tiles
+   */
+  void printTileList(vector<Tiles> tileList) {
+    for (int i = 0; i < tileList.size(); i++) {
+      cout << "Tuile " << tileList[i].id + 1 << endl;
+      for (int j = 0; j < tileList[i].tile.size(); j++) {
+        for (int k = 0; k < tileList[i].tile[0].size(); k++) {
+          if (tileList[i].tile[j][k] == 1) {
+            cout << "🟩";
+          } else {
+            cout << "⬜️";
+          }
+        }
+        cout << endl;
+      }
+      cout << endl;
+    }
+  }
+};
+
+/**
+ * @brief Create a Players object
+ *
+ * @return vector<Player>
+ */
 vector<Player> createPlayers() {
   int nbJoueurs;
   cout << "Enter the number of players (between 2 and 9)" << endl;
@@ -189,8 +284,12 @@ int main() {
 
   // vector<Player> players = createPlayers();
 
-  Board board(9);
-  board.printBord();
+  // Board board(9);
+  // board.printBord();
 
-    return 0;
+  LayingGrass layingGrass;
+  vector<LayingGrass::Tiles> tileList = layingGrass.createTileList();
+  layingGrass.printTileList(tileList);
+
+  return 0;
 }
