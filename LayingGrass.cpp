ee4427d557a8ebf5c4749042598b5877191efa6b
Le_Vrai_WIWI <superwoup@gmail.com>
2023-11-21 11:40:25 +0100

    player can chose their color

--- LayingGrass.cpp
+++ LayingGrass.cpp
@@ -12,13 +12,13 @@ class Player {
   string name;
   int couponEchange;
 
+  /**
+   * @brief Create a Player object
+   */
   void createPlayer() {
+    couponEchange = 1;
     cout << "Enter the player's name : ";
     cin >> name;
-    cout << "Enter the number of exchange coupons : ";
-    cin >> couponEchange;
-    cout << "Player " << name << " has been created with " << couponEchange
-         << " exchange coupons" << endl;
   }
 };
 
@@ -26,165 +26,11 @@ class Tuile {
  public:
   vector<vector<int>> tuile;
 
-  void creerTuile() {
-    srand(time(NULL));
-    for (int i = 0; i < 6; i++) {
-      tuile.push_back(vector<int>());
-      for (int j = 0; j < 6; j++) {
-        tuile[i].push_back(rand() % 2);
-      }
-    }
-  }
-
-  // fonction qui retourne une tuile
-  void flipTuile() { reverse(tuile.begin(), tuile.end()); }
-
-  // fonction qui tourne la tuile
-  void rotateTuile() {
-    // Transposer la matrice
-    for (size_t i = 0; i < tuile.size(); ++i) {
-      for (size_t j = i + 1; j < tuile[i].size(); ++j) {
-        swap(tuile[i][j], tuile[j][i]);
-      }
-    }
-    // Inverser l'ordre des colonnes
-    for (size_t i = 0; i < tuile.size(); ++i) {
-      reverse(tuile[i].begin(), tuile[i].end());
-    }
-  }
-
-  void afficherTuile() {
-    for (int i = 0; i < tuile.size(); i++) {
-      for (int j = 0; j < tuile[0].size(); j++) {
-        if (tuile[i][j] == 1) {
-          cout << "";
-        } else {
-          cout << " ";
-        }
-      }
-      cout << endl;
-    }
-    cout << endl;
-  }
-};
-
-class Board {
- public:
-  vector<vector<int>> cases;
-  int size = 20;
-
-  /**
-   * @brief Construct a new Board object
-   *
-   * @param nbPlayers
-   */
-  Board(int nbPlayers) {
-    if (nbPlayers > 4) {
-      size = 30;
-    }
-
-    for (int i = 0; i < size; i++) {
-      cases.push_back(vector<int>());
-      for (int j = 0; j < size; j++) {
-        cases[i].push_back(100);
-      }
-    }
-
-    // on place les tuiles de départ des joueurs aléatoirement
-    srand(time(NULL));
-    vector<pair<int, int>> positions;  // Stocke les positions déjà utilisées
-
-    for (int i = 0; i < nbPlayers; i++) {
-      int x, y;
-      do {
-        x = rand() % size;
-        y = rand() % size;
-      } while (find(positions.begin(), positions.end(), make_pair(x, y)) !=
-               positions.end());
-
-      cases[x][y] = i;
-      positions.push_back(make_pair(x, y));
-    }
-  }
-
-  // liste des carrés de couleurs
-  // ⬛️ ⬜️ 🟩 🟦 🟨 🟧 🟥 🟫 🟪 | 🔳 | ❌
-
-  /**
-   * @brief print the board
-   */
-  void printBord() {
-    cout << "   ";
-    for (int col = 0; col < cases[0].size(); col++) {
-      if (col < 10) {
-        cout << col << " ";
-      } else {
-        cout << col;
-      }
-    }
-    cout << endl;
-
-    for (int i = 0; i < cases.size(); i++) {
-      if (i < 10) {
-        cout << i << "  ";
-      } else {
-        cout << i << " ";
-      }
-      for (int j = 0; j < cases[0].size(); j++) {
-        switch (cases[i][j]) {
-          case 0:
-            cout << "🟥";
-            break;
-          case 1:
-            cout << "🟩";
-            break;
-          case 2:
-            cout << "🟦";
-            break;
-          case 3:
-            cout << "🟨";
-            break;
-          case 4:
-            cout << "🟪";
-            break;
-          case 5:
-            cout << "🟧";
-            break;
-          case 6:
-            cout << "⬜️";
-            break;
-          case 7:
-            cout << "🟫";
-            break;
-          case 8:
-            cout << "⬛️";
-            break;
-          default:
-            cout << "🔳";
-            break;
-        }
-      }
-      cout << endl;
-    }
-    cout << endl;
-  }
-};
-
-/**
- * @brief this class is used to manage the tiles
- *
- * @param id the id of the tile
- * @param tile the tile matrix
- * @param playerName the name of the player who place the tile
- *
- * @return vector<Tiles> the list of tiles
- */
-class LayingGrass {
  public:
   struct Tiles {
     int id;
-    vector<vector<int>> tile;
     string playerName;
+    vector<vector<int>> tile;
   };
 
   /**
@@ -266,7 +112,7 @@ class LayingGrass {
    *
    * @param tileList the list of tiles
    */
-  void printTileList(vector<Tiles> tileList) {
+  void printTilesListDebug(vector<Tiles> tileList) {
     for (int i = 0; i < tileList.size(); i++) {
       cout << "Tuile " << tileList[i].id + 1 << " de " << tileList[i].playerName
            << endl;
@@ -283,7 +129,154 @@ class LayingGrass {
       cout << endl;
     }
   }
+
+  /**
+   * @brief flipe the tile
+   */
+  void flipTuile() { reverse(tuile.begin(), tuile.end()); }
+
+  /**
+   * @brief rotate the tile 90° clockwise
+   *
+   */
+  void rotateTuile() {
+    // Transposer la matrice
+    for (size_t i = 0; i < tuile.size(); ++i) {
+      for (size_t j = i + 1; j < tuile[i].size(); ++j) {
+        swap(tuile[i][j], tuile[j][i]);
+      }
+    }
+    // Inverser l'ordre des colonnes
+    for (size_t i = 0; i < tuile.size(); ++i) {
+      reverse(tuile[i].begin(), tuile[i].end());
+    }
+  }
+
+  void afficherTuile() {
+    for (int i = 0; i < tuile.size(); i++) {
+      for (int j = 0; j < tuile[0].size(); j++) {
+        if (tuile[i][j] == 1) {
+          cout << "";
+        } else {
+          cout << " ";
+        }
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
 };
+
+class Board {
+ public:
+  vector<vector<int>> cases;
+  int size = 20;
+
+  /**
+   * @brief Construct a new Board object
+   *
+   * @param nbPlayers
+   */
+  Board(int nbPlayers) {
+    if (nbPlayers > 4) {
+      size = 30;
+    }
+
+    for (int i = 0; i < size; i++) {
+      cases.push_back(vector<int>());
+      for (int j = 0; j < size; j++) {
+        cases[i].push_back(100);
+      }
+    }
+
+    // on place les tuiles de départ des joueurs aléatoirement
+    srand(time(NULL));
+    vector<pair<int, int>> positions;  // Stocke les positions déjà utilisées
+
+    for (int i = 0; i < nbPlayers; i++) {
+      int x, y;
+      do {
+        x = rand() % size;
+        y = rand() % size;
+      } while (find(positions.begin(), positions.end(), make_pair(x, y)) !=
+               positions.end());
+
+      cases[x][y] = i;
+      positions.push_back(make_pair(x, y));
+    }
+  }
+
+  /**
+   * @brief print the board
+   */
+  void printBord() {
+    cout << endl;
+    cout << "   ";
+    for (int col = 0; col < cases[0].size(); col++) {
+      if (col < 10) {
+        cout << col << " ";
+      } else {
+        cout << col;
+      }
+    }
+    cout << endl;
+
+    for (int i = 0; i < cases.size(); i++) {
+      if (i < 10) {
+        cout << i << "  ";
+      } else {
+        cout << i << " ";
+      }
+      for (int j = 0; j < cases[0].size(); j++) {
+        switch (cases[i][j]) {
+          case 0:
+            cout << "🟥";
+            break;
+          case 1:
+            cout << "🟩";
+            break;
+          case 2:
+            cout << "🟦";
+            break;
+          case 3:
+            cout << "🟨";
+            break;
+          case 4:
+            cout << "🟪";
+            break;
+          case 5:
+            cout << "🟧";
+            break;
+          case 6:
+            cout << "⬜️";
+            break;
+          case 7:
+            cout << "🟫";
+            break;
+          case 8:
+            cout << "⬛️";
+            break;
+          default:
+            cout << "🔳";
+            break;
+        }
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
+};
+
+/**
+ * @brief this class is used to manage the tiles
+ *
+ * @param id the id of the tile
+ * @param tile the tile matrix
+ * @param playerName the name of the player who place the tile
+ *
+ * @return vector<Tiles> the list of tiles
+ */
+class LayingGrass {};
 
 /**
  * @brief Create the Players
@@ -316,14 +309,14 @@ int main() {
 
   system("clear");
 
-  // vector<Player> players = createPlayers();
+  vector<Player> players = createPlayers();
 
-  Board board(9);
+  Board board(players.size());
   board.printBord();
 
-  LayingGrass layingGrass;
-  vector<LayingGrass::Tiles> tileList = layingGrass.createTileList();
-  layingGrass.printTileList(tileList);
+  // LayingGrass layingGrass;
+  // vector<LayingGrass::Tiles> tileList = layingGrass.createTileList();
+  // layingGrass.printTilesListDebug(tileList);
 
   return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,25 +1,181 @@
+// clang++ LayingGrass.cpp -std=c++11
+#include <algorithm>
 #include <iostream>
+#include <vector>
+
+#include "tiles.cpp"
 using namespace std;
 
-int main() {
-  cout << "\033[0;32;44m text \033[0m\n" << endl;
-  return 0;
+class Player {
+ public:
+  string name;
+  int couponEchange;
+  int color;
+
+  Player(vector<string> availableColors) {
+    name = choseName();
+    couponEchange = 1;
+    color = choseColor(availableColors);
+  }
+
+  string choseName() {
+    cout << "Enter the player's name : ";
+    cin >> name;
+    return name;
+  }
+
+  int choseColor(vector<string> availableColors) {
+    for (int i = 0; i < availableColors.size(); i++) {
+      cout << i + 1 << " : " << availableColors[i] << endl;
+    }
+    cout << "Enter the player's color : ";
+    cin >> color;
+    while (color < 1 || color > availableColors.size() ||
+           availableColors[color - 1] == "❌") {
+      for (int i = 0; i < availableColors.size(); i++) {
+        cout << i + 1 << " : " << availableColors[i] << endl;
+      }
+      cout << "Enter the player's color : ";
+      cin >> color;
+    }
+    color--;
+    return color;
+  }
+
+  string getName() { return name; };
+  int getCouponEchange() { return couponEchange; };
+
+  string getColor(string arg = "emoji") {
+    vector<string> colors = {"⬛️", "⬜️", "🟩", "🟦", "🟨",
+                             "🟧", "🟥", "🟫", "🟪"};
+    if (arg == "int") {
+      return to_string(color);
+    } else {
+      return colors[color];
+    }
+  }
+};
+
+class Board {
+ public:
+  vector<vector<int>> cases;
+  int size = 20;
+
+  Board(int nbPlayers) {
+    if (nbPlayers > 4) {
+      size = 30;
+    }
+
+    for (int i = 0; i < size; i++) {
+      cases.push_back(vector<int>());
+      for (int j = 0; j < size; j++) {
+        cases[i].push_back(100);
+      }
+    }
+
+    // on place les tuiles de départ des joueurs aléatoirement
+    srand(time(NULL));
+    vector<pair<int, int>> positions;  // Stocke les positions déjà utilisées
+
+    for (int i = 0; i < nbPlayers; i++) {
+      int x, y;
+      do {
+        x = rand() % size;
+        y = rand() % size;
+      } while (find(positions.begin(), positions.end(), make_pair(x, y)) !=
+               positions.end());
+
+      cases[x][y] = i;
+      positions.push_back(make_pair(x, y));
+    }
+  }
+
+  void printBord() {
+    cout << endl;
+    // affichage des numéros de colonnes
+    cout << "   ";
+    for (int col = 0; col < cases[0].size(); col++) {
+      if (col < 10) {
+        cout << col << " ";
+      } else {
+        cout << col;
+      }
+    }
+    cout << endl;
+    // affichage des numéros de lignes et des cases
+    for (int i = 0; i < cases.size(); i++) {
+      if (i < 10) {
+        cout << i << "  ";
+      } else {
+        cout << i << " ";
+      }
+      for (int j = 0; j < cases[0].size(); j++) {
+        switch (cases[i][j]) {
+          case 0:
+            cout << "🟥";
+            break;
+          case 1:
+            cout << "🟩";
+            break;
+          case 2:
+            cout << "🟦";
+            break;
+          case 3:
+            cout << "🟨";
+            break;
+          case 4:
+            cout << "🟪";
+            break;
+          case 5:
+            cout << "🟧";
+            break;
+          case 6:
+            cout << "⬜️";
+            break;
+          case 7:
+            cout << "🟫";
+            break;
+          case 8:
+            cout << "⬛️";
+            break;
+          default:
+            cout << "🔳";
+            break;
+        }
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
+};
+
+vector<Player> createPlayers() {
+  int nbPlayers;
+  vector<string> colors = {"⬛️", "⬜️", "🟩", "🟦", "🟨",
+                           "🟧", "🟥", "🟫", "🟪"};
+  cout << "Enter the number of players (2-9) : ";
+  cin >> nbPlayers;
+  while (nbPlayers < 2 || nbPlayers > 9) {
+    cout << "Enter the number of players (2-9) : ";
+    cin >> nbPlayers;
+  }
+  vector<Player> players;
+  for (int i = 0; i < nbPlayers; i++) {
+    Player player(colors);
+    cout << endl;
+    colors[stoi(player.getColor("int"))] = "❌";
+    players.push_back(player);
+  }
+  return players;
 }
 
-//          foreground background
-// black        30         40
-// red          31         41
-// green        32         42
-// yellow       33         43
-// blue         34         44
-// magenta      35         45
-// cyan         36         46
-// white        37         47
+int main() {
+  system("clear");
+  vector<Player> players = createPlayers();
 
-// reset             0  (everything back to normal)
-// bold/bright       1  (often a brighter shade of the same colour)
-// underline         4
-// inverse           7  (swap foreground and background colours)
-// bold/bright off  21
-// underline off    24
-// inverse off      27
+  // debug
+  for (int i = 0; i < players.size(); i++) {
+    cout << players[i].name << " : " << players[i].getColor("int") << endl;
+  }
+  return 0;
+}
